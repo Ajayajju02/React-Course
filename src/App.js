ad451034c042030593c9fb53d50a528daa41ef07
Ajay Kumar <142593222+Ajayajju02@users.noreply.github.com>
2024-11-10 11:53:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Main from './Main'
 import Project1 from './Project1'
 import Project2 from './Project2'
+import Project3 from './Project3'
 import Table from './Table'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Main />
       <Project1 />
       <Project2 />
+      <Project3 />
       <Table />
     </div>
   )
